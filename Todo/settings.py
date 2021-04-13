daa6d135fa98e7d90669670289efeb5eefddcd74
PrashanjeetH <jeet8halder@gmail.com>
2021-04-13 15:33:52 +0530

    modified Settings.py

--- Todo/settings.py
+++ Todo/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os 
-Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -146,6 +146,6 @@ STATIC_URL = '/static/'
 STATIC_FILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage2'
 
 django_heroku.settings(locals())
